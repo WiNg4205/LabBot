670d4a2a38f0dc27c3f9bfa2610e46c7a7fcb565
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-01-18 18:14:15 +1100

    linting fixes

--- backend/data.js
+++ backend/data.js
@@ -1,119 +1,119 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
 
 import mongoose from 'mongoose'
+dotenv.config()
 const uri = `mongodb+srv://Scientists:${process.env.MONGO_PWD}@cluster0.qivnnso.mongodb.net/resultsdb2?retryWrites=true&w=majority`
-mongoose.connect(uri);
+mongoose.connect(uri)
 
 const playerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    winRate: { type: Number, default: 0}
-});
-const Player = mongoose.model('Player', playerSchema);
-const player = new Player({ name: "William" });
-await player.save();
+  name: { type: String, required: true },
+  winRate: { type: Number, default: 0 }
+})
+const Player = mongoose.model('Player', playerSchema)
+const player = new Player({ name: 'William' })
+await player.save()
 
 // const client = new MongoClient(uri);
 // const db = client.db("resultsdb");
 // const kittens = await Kitten.find();
 // console.log(kittens);
 class DatabaseHandler {
-    async initPlayerCollection() {
-        const playerSchema = {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["name"],
-                properties: {
-                    name: {
-                        bsonType: "string",
-                    },
-                    winRate: {
-                        bsonType: ["double"],
-                        description: "not set initially",
-                    },
-                },
-            },
-        };
-
-        const outingSchema = {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["people", "location", "restaurant", "date", "time"],
-                properties: {
-                    people: {
-                        bsonType: "array",
-                    },
-                    location: {
-                        bsonType: "string",
-                    },
-                    restaurant: {
-                        bsonType: "string",
-                    },
-                    activity: {
-                        bsonType: ["string"],
-                    },
-                    date: {
-                        bsonType: "string",
-                        description: "format should be YYYY-MM-DD",
-                    },
-                    time: {
-                        bsonType: "string",
-                    },
-                    games: {
-                        bsonType: "array",
-                    },
-                },
-            },
-        };
-
-        const gameSchema = {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["game", "teams", "results"],
-                properties: {
-                    game: {
-                        bsonType: "string",
-                        description: "game that we played (pool etc.)",
-                    },
-                    results: {
-                        bsonType: "array",
-                        items: {
-                            bsonType: "object",
-                            required: ["team", "placing"],
-                            properties: {
-                                team: {
-                                    bsonType: "array",
-                                    description: "team members"
-                                },
-                                placing: {
-                                    bsonType: "int",
-                                    description: "rank"
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-        };
-
-        // await db.createCollection("players", { validator: playerSchema });
-        await db.createCollection("outings", { validator: outingSchema });
-        // await db.createCollection("games", { validator: gameSchema });
+  async initPlayerCollection () {
+    const playerSchema = {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: ['name'],
+        properties: {
+          name: {
+            bsonType: 'string'
+          },
+          winRate: {
+            bsonType: ['double'],
+            description: 'not set initially'
+          }
+        }
+      }
     }
 
-    async closeClient() {
-        this.client.close();
+    const outingSchema = {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: ['people', 'location', 'restaurant', 'date', 'time'],
+        properties: {
+          people: {
+            bsonType: 'array'
+          },
+          location: {
+            bsonType: 'string'
+          },
+          restaurant: {
+            bsonType: 'string'
+          },
+          activity: {
+            bsonType: ['string']
+          },
+          date: {
+            bsonType: 'string',
+            description: 'format should be YYYY-MM-DD'
+          },
+          time: {
+            bsonType: 'string'
+          },
+          games: {
+            bsonType: 'array'
+          }
+        }
+      }
     }
 
-    async insertData() {
-        // const doc = { game: "pool", teams: [["Kevin", "William"], ["Brian", "William"]], results: [ {team: ["Brian, William"], placing: 1} ] };
-        const doc = { people: ["Kevin"], location: "Somewhere", restaurant: "A restaurant", activity: "pool", date: "2023-01-03", time: "6pm", games: ["65851d6d430377f2f6f43e0a"] };
-        const myColl = db.collection("outings");
-        myColl.insertOne(doc);
+    const gameSchema = {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: ['game', 'teams', 'results'],
+        properties: {
+          game: {
+            bsonType: 'string',
+            description: 'game that we played (pool etc.)'
+          },
+          results: {
+            bsonType: 'array',
+            items: {
+              bsonType: 'object',
+              required: ['team', 'placing'],
+              properties: {
+                team: {
+                  bsonType: 'array',
+                  description: 'team members'
+                },
+                placing: {
+                  bsonType: 'int',
+                  description: 'rank'
+                }
+              }
+            }
+          }
+        }
+      }
     }
+
+    // await db.createCollection("players", { validator: playerSchema });
+    await db.createCollection('outings', { validator: outingSchema })
+    // await db.createCollection("games", { validator: gameSchema });
+  }
+
+  async closeClient () {
+    this.client.close()
+  }
+
+  async insertData () {
+    // const doc = { game: "pool", teams: [["Kevin", "William"], ["Brian", "William"]], results: [ {team: ["Brian, William"], placing: 1} ] };
+    const doc = { people: ['Kevin'], location: 'Somewhere', restaurant: 'A restaurant', activity: 'pool', date: '2023-01-03', time: '6pm', games: ['65851d6d430377f2f6f43e0a'] }
+    const myColl = db.collection('outings')
+    myColl.insertOne(doc)
+  }
 }
 
-const databaseHandler = new DatabaseHandler();
+const databaseHandler = new DatabaseHandler()
 
-export default databaseHandler;
+export default databaseHandler
--- backend/main.js
+++ backend/main.js
@@ -1,100 +1,88 @@
-import { Client, IntentsBitField } from "discord.js";
-import dotenv from "dotenv";
-import apiHandler from "./api.js";
-import minigameHandler from "./minigame.js";
-import databaseHandler from "./data.js";
+import { Client, IntentsBitField } from 'discord.js'
+import dotenv from 'dotenv'
+import apiHandler from './api.js'
+import minigameHandler from './minigame.js'
+import databaseHandler from './data.js'
 
-dotenv.config();
+dotenv.config()
 
 const people = {
-    "287695357175922688" : "Brian",    
-    "393600653328515073" : "Kevin",
-    "435674199424499712" : "Jeremy",
-    "558581174629302273" : "William",  
-    "420151663429681153" : "Winson",
-};
+  '287695357175922688': 'Brian',
+  '393600653328515073': 'Kevin',
+  '435674199424499712': 'Jeremy',
+  '558581174629302273': 'William',
+  '420151663429681153': 'Winson'
+}
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
-});
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+})
 
+client.on('ready', () => {
+  console.log('Our bot is ready to go!')
+})
 
-client.on("ready", () => {
-    console.log("Our bot is ready to go!");
-});
+client.on('messageCreate', msg => {
+  if (msg.author.bot) return // To avoid infinite loops
 
-
-client.on("messageCreate", msg => {
-    if (msg.author.bot) return; // To avoid infinite loops
-
-
-    if (msg.content === "!q") {
-        minigameHandler.game = null;
-    } 
-    else if (minigameHandler.game != null) {
-        minigameHandler.play(msg.content).then(result => msg.channel.send(result));
-
-    } else if (msg.content === "Hello LabBot") {
-        const author = people[msg.author.id];
-        msg.channel.send(`Hello ${author}!`);
-
-    } else if (msg.content === "!inspire") {
-        apiHandler.getQuote().then(quote => msg.channel.send(quote));
-
-    } else if (msg.content === "!joke") {
-        apiHandler.getJoke().then(joke => msg.channel.send(joke))
-        .then(sentMessage => sentMessage.react("🤣"));
-
-    } else if (msg.content === "!weather") {
-        apiHandler.getWeather().then(weather => msg.channel.send(weather));
-
-    } else if (msg.content === "!guess") {
-        minigameHandler.playGuessTheNumber().then(guess => msg.channel.send(guess));
-    
-    } else if (msg.content === "!initdb") {
-        databaseHandler.initPlayerCollection();
-    
-    } else if (msg.content === "!addData") {
-        databaseHandler.insertData();
-
-    } else if (msg.content === "!createEvent") {
-        // TODO:
-    } else if (msg.content === "!event") {
-        // TODO:
-    } else if (msg.content.startsWith("Name = ") ||
-               msg.content.startsWith("Date = ") ||
-               msg.content.startsWith("Time = ") ||
-               msg.content.startsWith("Restaurant = ") ||
-               msg.content.startsWith("Activity = ")) {
-        // TODO:
-    } else if (msg.content === "!initialise") {
-        // TODO:
-    } else if (msg.content.startsWith("!addPlayer")) {
-        // TODO:
-    } else if (msg.content.startsWith("!getPlayer")) {
-        // TODO:
-    } else if (msg.content.startsWith("!getTeam")) {
-        // TODO:
-    } else if (msg.content.startsWith("!gameResult")) {
-        // TODO:
-    } else if (msg.content.startsWith("!winrate")) {
-        // TODO:
-    } else if (msg.content === "!bestTeam") {
-        // TODO:
-    } else if (msg.content === "!worstTeam") {
-        // TODO:
-    }
-});
+  if (msg.content === '!q') {
+    minigameHandler.game = null
+  } else if (minigameHandler.game != null) {
+    minigameHandler.play(msg.content).then(result => msg.channel.send(result))
+  } else if (msg.content === 'Hello LabBot') {
+    const author = people[msg.author.id]
+    msg.channel.send(`Hello ${author}!`)
+  } else if (msg.content === '!inspire') {
+    apiHandler.getQuote().then(quote => msg.channel.send(quote))
+  } else if (msg.content === '!joke') {
+    apiHandler.getJoke().then(joke => msg.channel.send(joke))
+      .then(sentMessage => sentMessage.react('🤣'))
+  } else if (msg.content === '!weather') {
+    apiHandler.getWeather().then(weather => msg.channel.send(weather))
+  } else if (msg.content === '!guess') {
+    minigameHandler.playGuessTheNumber().then(guess => msg.channel.send(guess))
+  } else if (msg.content === '!initdb') {
+    databaseHandler.initPlayerCollection()
+  } else if (msg.content === '!addData') {
+    databaseHandler.insertData()
+  } else if (msg.content === '!createEvent') {
+    // TODO:
+  } else if (msg.content === '!event') {
+    // TODO:
+  } else if (msg.content.startsWith('Name = ') ||
+               msg.content.startsWith('Date = ') ||
+               msg.content.startsWith('Time = ') ||
+               msg.content.startsWith('Restaurant = ') ||
+               msg.content.startsWith('Activity = ')) {
+    // TODO:
+  } else if (msg.content === '!initialise') {
+    // TODO:
+  } else if (msg.content.startsWith('!addPlayer')) {
+    // TODO:
+  } else if (msg.content.startsWith('!getPlayer')) {
+    // TODO:
+  } else if (msg.content.startsWith('!getTeam')) {
+    // TODO:
+  } else if (msg.content.startsWith('!gameResult')) {
+    // TODO:
+  } else if (msg.content.startsWith('!winrate')) {
+    // TODO:
+  } else if (msg.content === '!bestTeam') {
+    // TODO:
+  } else if (msg.content === '!worstTeam') {
+    // TODO:
+  }
+})
 
 process.on('SIGINT', () => {
-    console.log('Received SIGINT. Closing bot...');
-    databaseHandler.closeClient(); // close database client before Ctrl+C in development
-    process.exit();
-});
+  console.log('Received SIGINT. Closing bot...')
+  databaseHandler.closeClient() // close database client before Ctrl+C in development
+  process.exit()
+})
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
