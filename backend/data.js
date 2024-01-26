f908683aa65805aad02ba3c3e6719964fd5045a6
Kevin Kong <kkon0188@uni.sydney.edu.au>
2024-01-26 15:09:22 +1100

    merge

--- backend/data.js
+++ backend/data.js
@@ -1,74 +1,76 @@
 import dotenv from 'dotenv'
-dotenv.config();
-
+import { ActionRowBuilder, TextInputStyle, ComponentType, SlashCommandBuilder} from 'discord.js'
 import mongoose, { trusted } from 'mongoose'
-const { Schema } = mongoose;
+dotenv.config()
+const { Schema } = mongoose
 const uri = `mongodb+srv://Scientists:${process.env.MONGO_PWD}@cluster0.qivnnso.mongodb.net/results?retryWrites=true&w=majority`
-mongoose.connect(uri);
+mongoose.connect(uri)
 
-class databaseHandler {
-    static playerSchema = new mongoose.Schema({
-        name: { 
-            type: String, 
-            required: true 
-        },
-        winRate: { 
-            type: Number,
-            default: 0
-        }
-    });
+class DatabaseHandler {
+  static playerSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    winRate: {
+      type: Number,
+      default: 0
+    }
+  })
 
-    static resultsSchema = new mongoose.Schema({ 
-        placing: {
-            type: Number,
-            required: true
-        },
-        team: {
-            type: [String],
-            required: true
-        }
-     });
+  static resultsSchema = new mongoose.Schema({
+    placing: {
+      type: Number,
+      required: true
+    },
+    team: {
+      type: [String],
+      required: true
+    }
+  })
 
-    static outingSchema = new mongoose.Schema({
-        people: {
-            type: [String],
-            required: true
-        },
-        location: {
-            type: String,
-            required: true
-        },
-        restaurant: {
-            type: String, 
-            required: true
-        },
-        date: {
-            type: Date,
-            min: '2024-01-20'
-        },
-        games: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Game'
-            }
-        ]
-    });
+  static outingSchema = new mongoose.Schema({
+    people: {
+      type: [String],
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    restaurant: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Date,
+      min: '2024-01-20'
+    },
+    games: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Game'
+      }
+    ]
+  })
 
-    static gameSchema = new mongoose.Schema({
-        game: {
-            type: String,
-            required: true
-        },
-        result: {
-            type: [databaseHandler.resultsSchema],
-        }
-    });
+  static gameSchema = new mongoose.Schema({
+    game: {
+      type: String,
+      required: true
+    },
+    result: {
+      type: [DatabaseHandler.resultsSchema]
+    }
+  })
+
+  constructor() {
+    this.Player = mongoose.model('Player', DatabaseHandler.playerSchema)
+    this.Game = mongoose.model('Game', DatabaseHandler.gameSchema)
+    this.Outing = mongoose.model('Outing', DatabaseHandler.outingSchema)
+    this.Result = mongoose.model('Result', DatabaseHandler.resultsSchema)
+  }
 }
-
-const Player = mongoose.model('Player', databaseHandler.playerSchema);
-const Game = mongoose.model('Game', databaseHandler.gameSchema);
-const Outing = mongoose.model('Outing', databaseHandler.outingSchema);
-const Result = mongoose.model('Result', databaseHandler.resultsSchema);
 
 // const result = new Result({
 //     placing: "1",
@@ -96,13 +98,15 @@ const Result = mongoose.model('Result', databaseHandler.resultsSchema);
 //     games: ["65af3ed19862e629edfba09e", "65af3ed19862e629edfba0a3"],
 // });
 
-// // const results = await Game.find({}).
-// //     populate('result').
-// //     exec();
+// const results = await Game.find({}).
+//     populate('result').
+//     exec();
 
 // const outing1 = await Outing.findOne({}).
 //     populate('games').
 //     exec();
 // console.log(outing1.games[0].result);
 
-export default databaseHandler;
+const databaseHandler = new DatabaseHandler()
+
+export default databaseHandler
