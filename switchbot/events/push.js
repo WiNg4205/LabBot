245ef7012cc8dd9d4d645d554c34d733a86acee5
WiNg4205 <william.nguyen4205@gmail.com>
2025-04-17 16:01:48 +1000

    added pull script to switchbot event

--- switchbot/events/push.js
+++ switchbot/events/push.js
@@ -1,10 +1,11 @@
 import { Events } from 'discord.js'
+import { exec } from 'child_process'
 
 const pull = {
 	name: Events.MessageCreate,
 	execute (message) {
 		if (message.author.id === "1111618687032627203") {
-            console.log("pull");
+            exec('gnome-terminal -- bash -c "./pull.sh"');
         }
 		
 	}
