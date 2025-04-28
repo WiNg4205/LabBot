0205413c1ab3a8836abedf53249fcd0450b4534b
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-04-28 22:49:46 +1000

    winrate command done

--- backend/commands/utility/winrate.js
+++ backend/commands/utility/winrate.js
@@ -1,27 +1,64 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
+import { SlashCommandBuilder, EmbedBuilder, MessageFlags } from 'discord.js'
 import databaseHandler from '../../data.js'
+
+async function getWinRateByGame (game) {
+  const results = []
+  const players = await databaseHandler.Player.find()
+  players.forEach(player => {
+    results.push({ name: player.name, points: 0, winRate: 0 })
+  })
+
+  // Calculate game win rate for each player
+  const games = await databaseHandler.Game.find({ game })
+  games.forEach(game => {
+    game.results.forEach((value, key) => {
+      results[results.findIndex(player => player.name === key)].points += value
+    })
+  })
+  results.forEach(player => {
+    player.winRate = ((player.points / games.length) * 100).toFixed(2)
+  })
+
+  return results
+}
 
 const winrateCommand = {
   data: new SlashCommandBuilder()
     .setName('winrate')
-    .setDescription("Returns everyone's win rate!"),
+    .setDescription("Returns everyone's win rate!")
+    .addStringOption(option =>
+      option.setName('game')
+        .setDescription('game')
+        .setRequired(false)
+        .addChoices(
+          { name: 'Pool', value: 'pool' },
+          { name: 'Cards', value: 'cards' },
+          { name: 'Bowling', value: 'bowling' }
+        )),
   async execute (interaction) {
     const embed = new EmbedBuilder()
       .setColor(0x0099FF)
       .setTitle('Win rate')
-      .setAuthor({ name: interaction.user.globalName, iconURL: interaction.user.displayAvatarURL() })
-      .setDescription('Overall win rate across all games')
+      .addFields({ name: '\u200B', value: '\u200B' })
       .setThumbnail(interaction.guild.iconURL())
       .setTimestamp()
-      .setFooter({ text: ' ', iconURL: interaction.user.displayAvatarURL() })
 
-    const players = await databaseHandler.Player.find()
-    for (let i = 0; i < players.length; i++) {
-      const player = players[i]
-      let winRate = (player.gamesWon / player.gamesPlayed) * 100
-      winRate = winRate.toFixed(2)
-
-      embed.addFields({ name: player.name, value: winRate + '%' })
+    let game
+    if (interaction.options.getString('game')) {
+      game = interaction.options.getString('game')
+      embed.setDescription(`Win rate for ${game}`)
+      const playerWinRates = await getWinRateByGame(game)
+      for (let i = 0; i < playerWinRates.length; i++) {
+        embed.addFields({ name: playerWinRates[i].name, value: playerWinRates[i].winRate + '%' })
+      }
+    } else {
+      embed.setDescription('Win rate across all games')
+      // Get players overall win rate
+      const players = await databaseHandler.Player.find()
+      for (let i = 0; i < players.length; i++) {
+        const winRate = ((players[i].gamesWon / players[i].gamesPlayed) * 100).toFixed(2)
+        embed.addFields({ name: players[i].name, value: winRate + '%' })
+      }
     }
 
     interaction.reply({ embeds: [embed] })
