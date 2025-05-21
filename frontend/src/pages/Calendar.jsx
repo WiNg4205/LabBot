7896aa5db8563ece44439840f01ff1d3db08d69a
WiNg4205 <william.nguyen4205@gmail.com>
2025-05-21 21:31:28 +1000

    added unstyled calendar (may change to different package)

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,10 +1,31 @@
 import NavBar from "../components/NavBar"
+import { useEffect, useRef } from "react"
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css"
 
 const Calendar = () => {
+  const inputRef = useRef(null)
+  const fpInstance = useRef(null)
+
+  useEffect(() => {
+    fpInstance.current = flatpickr(inputRef.current, {
+      inline: true,
+      onReady: (selectedDates, dateStr, instance) => {
+        instance.input.style.display = "none"
+      },
+      dateFormat: "Y-m-d"
+    })
+    return () => {
+      fpInstance.current.destroy()
+    }
+  }, [])
+
   return <>
     <NavBar />
     <h1>Calendar</h1>
+    <input ref={inputRef} />
   </>
 }
 
 export default Calendar
+
