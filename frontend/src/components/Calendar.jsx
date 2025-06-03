cbbc8d840e8ffbf9775e8528acef5ac90c864a05
WiNg4205 <william.nguyen4205@gmail.com>
2025-06-03 23:28:31 +1000

    working date navigation with dummy data

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import { DayPicker, getDefaultClassNames } from "react-day-picker";
-import { addMonths, subMonths } from "date-fns";
+import { startOfMonth } from "date-fns";
+import dates from "../data/Events";
 import "./Calendar.css";
 
+
+
+
 const Calendar = () => {
-  const [month, setMonth] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(dates[0]);
+
   const defaultClassNames = getDefaultClassNames();
+
+  const prevDate = () => setSelectedDate(d => dates[Math.min(dates.indexOf(d) + 1, dates.length - 1)]);
+  const nextDate = () => setSelectedDate(d => dates[Math.max(dates.indexOf(d) - 1, 0)]);
 
   return (
     <div className="flex items-start gap-4">
-      <button onClick={() => setMonth(subMonths(month, 1))}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 mt-48 hover:text-purple-400">
+      <button onClick={prevDate}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 mt-40 hover:text-purple-400">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
         </svg>
       </button>
 
       <DayPicker
-        month={month}
-        onMonthChange={setMonth}
-        animate
+        key={startOfMonth(selectedDate).toISOString()}
+        month={startOfMonth(selectedDate)}
+        selected={selectedDate}
         mode="single"
         disableNavigation
         classNames={{
@@ -29,8 +37,8 @@ const Calendar = () => {
         }}
       />
 
-      <button onClick={() => setMonth(addMonths(month, 1))}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 mt-48 hover:text-purple-400">
+      <button onClick={nextDate}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 mt-40 hover:text-purple-400">
           <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
       </button>
