15b430e62e1b45b01ae31c84ecf417d3669f7b56
WiNg4205 <william.nguyen4205@gmail.com>
2025-06-04 16:50:57 +1000

    removed dummy data

--- frontend/src/data/Events.js
+++
@@ -1,8 +0,0 @@
-const date1 = new Date('2025-04-09T00:00:00.000Z')
-const date2 = new Date('2025-03-12T00:00:00.000Z')
-const date3 = new Date('2025-02-10T00:00:00.000Z')
-const date4 = new Date('2024-12-17T00:00:00.000Z')
-const date5 = new Date('2024-11-15T00:00:00.000Z')
-const dates = [date1, date2, date3, date4, date5]
-
-export default dates
