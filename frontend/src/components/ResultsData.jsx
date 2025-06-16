b0a921633b088a256cdb6dcbcb2950fa3ea27349
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-06-16 22:51:38 +1000

    added players for each game in results

--- frontend/src/components/ResultsData.jsx
+++ frontend/src/components/ResultsData.jsx
@@ -1,18 +1,38 @@
+import { useAvatars } from "../context/AvatarsContext"
+import names from "../utility/names"
+
 const ResultsData = ({ games }) => {
+
+  const getAvatars = useAvatars() || []
+  console.log("avatars: ", getAvatars)
+  
+  // games.map(game => {
+  //   Object.entries(game.results).map(result => {
+  //     console.log("result: ", result)
+  //   })
+  // })
 
   return (
     <div className="flex flex-col w-3/5 box-border">
       <h2 className="text-2xl tracking-wider font-extrabold my-2">RESULTS</h2>
       {games.map((game, index) => (
-        <div key={index} className="flex flex-row bg-zinc-800 rounded-md p-4 mb-2 border-l-6 min-h-24">
-          <div className="flex flex-col">
-            <p className="text-md font-bold">{game.game.charAt(0).toUpperCase() + game.game.slice(1)}</p>
-            <p className="text-md font-extralight">{new Date(game.date).toLocaleDateString()}</p>
+        <div key={index} className="flex justify-between bg-zinc-800 rounded-md p-4 mb-2 border-l-6 min-h-24 max-h-24">
+          <div className="flex flex-col justify-center">
+            <p className="text-sm font-semibold text-slate-100">{game.game.charAt(0).toUpperCase() + game.game.slice(1)}</p>
+            <p className="text-sm font-extralight">{new Date(game.date).toLocaleDateString()}</p>
           </div>
+          {getAvatars.length > 0 && <div className="grid grid-flow-col grid-rows-3 gap-6 items-center">
+            {Object.entries(game.results).map((result, index) => (
+               <a key={index} className="flex gap-x-2 min-w-28 max-w-28" href={''} aria-disabled="true">
+                <img src={getAvatars.find(avatar => avatar.username === names[result[0]])["avatar"]} alt="avatar" className="rounded-full size-6" />
+                <span className="text-sm truncate">{result[0]}</span>
+              </a>
+            ))}
+          </div>}
         </div>
       ))}
     </div>
   )
 }
 
-export default ResultsData
+export default ResultsData  
---
+++ frontend/src/utility/names.js
@@ -0,0 +1,10 @@
+const names = {
+  'Brian': 'berriican',
+  'Kevin': 'amizee',
+  'Jeremy': 'kindaphocus',
+  'Joseph': 'joeyyyyy', 
+  'William': 'wing4205',
+  'Winson': 'cainretros'
+}
+
+export default names
