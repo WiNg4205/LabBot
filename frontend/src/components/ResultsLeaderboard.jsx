ba22a5f317909c73cce8754cacbe87f1e333d0f6
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-06-19 12:01:42 +1000

    gradient styling for ranking

--- frontend/src/components/ResultsLeaderboard.jsx
+++ frontend/src/components/ResultsLeaderboard.jsx
@@ -23,11 +23,28 @@ const ResultsLeaderboard = ({ gameType }) => {
   return (
     <div className="flex flex-col w-2/5 mr-8">
       <h2 className="text-xl tracking-wider font-extrabold my-4">LEADERBOARD</h2>
-      <div className="flex flex-col bg-zinc-800 rounded-md border-[0.5px] border-gray-500">
+      <div className="flex flex-col bg-zinc-800 border-[0.5px] border-gray-500">
         {winrates.map((player, index) => (
           <div className="flex justify-between items-center">
-            <div className="flex items-center px-2 py-4 pl-4">
-              <span className="w-6">{index+1}</span>
+            <div className="flex items-center py-4 pl-4">
+              <div className={`rounded-full p-[3px] ${
+                    index === 0 ? "bg-gradient-to-b from-[#ebd197] from-30% via-[#b48811] via-50% to-[#a2790d] to-90%"
+                    : index === 1 ? "bg-gradient-to-b from-[#B3B6B5] from-30% via-[#8E8D8D] via-50% to-[#656564] to-90%"
+                    : index === 2 ? "bg-gradient-to-b from-[#B08D57] from-30% via-[#895E1A] via-50% to-[#804A00] to-90%"
+                    : ""
+                  }`
+                }
+              >
+                <div class="rounded-full bg-zinc-800 w-10 h-10 flex items-center justify-center">
+                  <span className={`rounded-full p-[3px] ${
+                      index === 0 ? "text-lg font-bold bg-gradient-to-b from-[#ebd197] from-30% via-[#b48811] via-50% to-[#a2790d] to-90% inline-block text-transparent bg-clip-text"
+                      : index === 1 ? "text-lg font-bold bg-gradient-to-b from-[#B3B6B5] from-30% via-[#8E8D8D] via-50% to-[#656564] to-90% inline-block text-transparent bg-clip-text"
+                      : index === 2 ? "text-lg font-bold bg-gradient-to-b from-[#B08D57] from-30% via-[#895E1A] via-50% to-[#804A00] to-90% inline-block text-transparent bg-clip-text" 
+                      : ""
+                    }`
+                  }>{index + 1}</span>
+                </div>
+              </div>
               <div>{ getAvatars.length > 0 && <img src={getAvatars.find(avatar => avatar.username === names[player.name]).avatar} alt="Avatar" className="rounded-full size-9 ml-4" /> }</div>
               <span className="ml-4">{ getAvatars.length > 0 && player.name }</span>
             </div>
