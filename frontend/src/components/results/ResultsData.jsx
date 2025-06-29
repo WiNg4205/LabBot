6b13e248073d83bd60def53d22b84a812a3403b6
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-06-29 23:13:23 +1000

    dropdown button

--- frontend/src/components/results/ResultsData.jsx
+++ frontend/src/components/results/ResultsData.jsx
@@ -1,14 +1,15 @@
 import { useAvatars } from "../../context/AvatarsContext"
 import names from "../../utility/names"
+import BottomArrow from "../../assets/bottom-arrow.svg"
 
 const ResultsData = ({ games }) => {
 
   const getAvatars = useAvatars() || []
-  console.log("avatars: ", getAvatars)
 
   return (
     <div className="flex flex-col w-3/5 box-border">
       <h2 className="text-xl tracking-wider font-extrabold my-4">RECENT GAMES</h2>
+
       {getAvatars.length <= 0 ? (
         Array.from({ length: 20 }).map((_, i) => (
           <div key={i} className="flex animate-pulse justify-between bg-zinc-800 rounded-md p-4 mb-1 border-l-6 border-l-zinc-600 min-h-24 max-h-24">
@@ -28,20 +29,25 @@ const ResultsData = ({ games }) => {
         ))
       ) : (
         games.map((game, index) => (
-          <div key={index} className="flex justify-between bg-zinc-800 rounded-md p-4 mb-1 border-l-6 border-l-zinc-600 min-h-24 max-h-24">
-            <div className="flex flex-col justify-center">
-              <p className="text-sm font-bold text-slate-100">{game.game.charAt(0).toUpperCase() + game.game.slice(1)}</p>
-              <p className="text-sm font-extralight text-slate-400">{new Date(game.date).toLocaleDateString()}</p>
+          <div key={index} className="flex bg-zinc-800 rounded-md mb-1 border-l-6 border-l-zinc-600 min-h-24 max-h-24">
+            <div className="flex flex-1 justify-between pl-4 py-4">
+              <div className="flex flex-col justify-center">
+                <p className="text-sm font-bold text-slate-100">{game.game.charAt(0).toUpperCase() + game.game.slice(1)}</p>
+                <p className="text-sm text-slate-400">{new Date(game.date).toLocaleDateString()}</p>
+              </div>
+              <div className="grid grid-flow-col grid-rows-3 gap-6 items-center">
+                {Object.entries(game.results).map((result, index) => (
+                  <a key={index} className="flex gap-x-2 min-w-28 max-w-28" href={''} aria-disabled="true">
+                    <img src={getAvatars.find(avatar => avatar.username === names[result[0]])["avatar"]} alt="avatar" className="rounded-full size-6" />
+                    <span className="text-sm truncate py-0.5">{result[0]}</span>
+                  </a>
+                ))}
+              </div>
             </div>
-            <div className="grid grid-flow-col grid-rows-3 gap-6 items-center">
-              {Object.entries(game.results).map((result, index) => (
-                <a key={index} className="flex gap-x-2 min-w-28 max-w-28" href={''} aria-disabled="true">
-                  <img src={getAvatars.find(avatar => avatar.username === names[result[0]])["avatar"]} alt="avatar" className="rounded-full size-6" />
-                  <span className="text-sm truncate py-0.5">{result[0]}</span>
-                </a>
-              ))}
+            <div className="flex flex-col justify-end bg-zinc-700 px-4 py-2 rounded-tr-md rounded-br-md cursor-pointer hover:bg-zinc-800 transition duration-300">
+              <img className="size-4" src={BottomArrow} alt="arrow"></img>
+            </div>
           </div>
-        </div>
         ))
       )}
     </div>
