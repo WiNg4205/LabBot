5e75ae7f6a3e14de37d05c6d62e513169207b3ba
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-07-02 20:00:41 +1000

    remove dropdown highlighting + leaderboard hover effect

--- frontend/src/components/results/ResultsDropdown.jsx
+++ frontend/src/components/results/ResultsDropdown.jsx
@@ -9,7 +9,7 @@ const ResultsDropdown = ({ id, selectedGame, setSelectedGame }) => {
 
   return (
     <>
-      <div className="flex flex-col justify-end bg-[#42424a] px-3 py-2 rounded-tr-md rounded-br-md cursor-pointer hover:bg-zinc-800 transition duration-300" onClick={handleClick}>
+      <div className="flex flex-col justify-end bg-[#42424a] px-3 py-2 rounded-tr-md rounded-br-md cursor-pointer hover:bg-zinc-800 transition duration-300 select-none" onClick={handleClick}>
         <img className="size-4" src={selectedGame === id ? TopArrow : BottomArrow} alt="arrow"></img>
       </div>
     </>
