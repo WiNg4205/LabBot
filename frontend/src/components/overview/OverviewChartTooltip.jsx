508d5f4a5a38d36657d87ea20ee275f4f7b84e1e
Kevin Kong <kkon0188@uni.sydney.edu.au>
2025-07-28 21:06:35 +1000

    tooltips

--- frontend/src/components/overview/OverviewChartTooltip.jsx
+++ frontend/src/components/overview/OverviewChartTooltip.jsx
@@ -3,9 +3,10 @@ const OverviewChartTooltip = ({ active, payload, label }) => {
   const data = payload[0].payload
 
   return (
-    <div className="bg-zinc-900/95 border border-zinc-800 rounded-md px-4 py-2">
-      <div className="text-base font-bold text-fuchsia-400">{label}</div>
-      <div className="text-slate-100 text-base mt-1">
+    <div className="bg-zinc-800 border border-zinc-700 rounded-md py-2">
+      <div className="text-base font-bold text-fuchsia-400 px-4">{label}</div>
+      <div className="h-[0.5px] bg-zinc-700 w-[90%] mx-auto mt-2 mb-1"></div>
+      <div className="text-slate-100 text-base mt-1 px-4">
         <span className="font-normal">Winrate: {(data.winRate * 1).toFixed(2)}%</span>
       </div>
     </div>
---
+++ frontend/src/components/overview/ProgressionChartTooltip.jsx
@@ -0,0 +1,19 @@
+const ProgressionChartTooltip = ({ active, payload, label }) => {
+  if (!active || !payload || !payload.length) return null
+  payload = payload.filter(payload => !isNaN(payload.value))
+  const sorted = [...payload].sort((a, b) => b.value - a.value)
+
+  return (
+    <div className="flex flex-col whitespace-nowrap absolute left-[650px] top-[100px] bg-zinc-800 py-2 rounded-md border border-zinc-700">
+      <span className="font-bold text-fuchsia-400 text-base px-4">{`Index`}</span>
+      <div className="h-[0.5px] bg-zinc-700 px-4 w-[90%] mx-auto mt-2 mb-1"></div>
+      {sorted.map(entry => (
+        <p key={entry.dataKey} style={{ color: entry.color }} className="mt-1 font-normal text-base px-4">
+          {entry.dataKey}: {entry.value}%
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export default ProgressionChartTooltip
